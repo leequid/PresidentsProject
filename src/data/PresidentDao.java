5d046192c2f6747375d86da4d7df10a481a7967f
Nancy Tran <nancytran@SDMacBookPro3-2.local>
2017-05-12 15:50:56 -0600

    created DAOs

---
+++ src/data/PresidentDao.java
@@ -0,0 +1,8 @@
+package data;
+
+import java.util.List;
+
+public interface PresidentDao {
+	List<President> getAllPresidents();
+	President getPresident();
+}
---
+++ src/data/PresidentDaoListImpl.java
@@ -0,0 +1,5 @@
+package data;
+
+public class PresidentDaoListImpl implements PresidentDao {
+
+}
